898ccca61dc8e63dc73d96b430f063fbfdeb2e6e
Henrik Grimler <henrik@grimler.se>
2021-11-02 20:01:51 +0100

    [SQUASHME] libllvm: try building without hostbuilt f18

--- packages/libllvm/build.sh
+++ packages/libllvm/build.sh
@@ -65,7 +65,7 @@ termux_step_host_build() {
 
 	cmake -G Ninja -DLLVM_ENABLE_PROJECTS='clang;flang;lldb;mlir' $TERMUX_PKG_SRCDIR/llvm
 	ninja -j $TERMUX_MAKE_PROCESSES clang-tblgen lldb-tblgen llvm-tblgen \
-				        mlir-tblgen mlir-linalg-ods-gen f18
+				        mlir-tblgen mlir-linalg-ods-gen
 }
 
 termux_step_pre_configure() {
