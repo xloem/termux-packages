de4b41e331f7760216ef413a48a211c463e57364
Henrik Grimler <henrik@grimler.se>
2021-11-17 22:12:28 +0100

    calc: patch Makefile instead of overriding a lot of variables
    And fix so that the package is installed to TERMUX_PKG_MASSAGEDIR.

--- packages/calc/build.sh
+++ packages/calc/build.sh
@@ -9,17 +9,4 @@ TERMUX_PKG_SHA256=6ae538f57785c5701a70112ccf007ab5553abd332ae2deaadaf564f401c734
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="ncurses, ncurses-ui-libs, readline"
 TERMUX_PKG_BUILD_IN_SRC=true
-
-termux_step_make() {
-	# Fails with -j$TERMUX_MAKE_PROCESSES for some reason
-	make USE_READLINE="-DUSE_READLINE" READLINE_EXTRAS="-lhistory -lncurses" \
-		READLINE_LIB="-L$TERMUX_PREFIX/lib -lreadline" \
-		READLINE_INCLUDE="-I$TERMUX_PREFIX/include" \
-		DEFAULT_LIB_INSTALL_PATH="$TERMUX_PREFIX/lib" CC="$CC" -j1 \
-		LONG_BITS=$TERMUX_ARCH_BITS
-}
-
-termux_step_make_install() {
-	make T="$TERMUX_PREFIX" BINDIR="/bin" LIBDIR="/lib" INCDIR="/include" \
-		SCRIPTDIR="/share/calc/cscript" CALC_SHAREDIR="/share/calc" install 
-}
+TERMUX_MAKE_PROCESSES=1
